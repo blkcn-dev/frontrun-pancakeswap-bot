a14e222e982e19428d27de616f2288efcbd21262
blkcn-dev <87132667+blkcn-dev@users.noreply.github.com>
2021-07-08 16:24:27 +0000

    Create env.js

---
+++ env.js
@@ -0,0 +1,12 @@
+const PRIVATE_KEY='Your private key here'
+
+const TOKEN_ADDRESS='0x2859e4544c4bb03966803b044a93563bd2d0dd4d'; // change to Token address you want to target
+const AMOUNT=0.01
+const LEVEL=0.1
+
+module.exports = {
+    PRIVATE_KEY,
+    TOKEN_ADDRESS,
+    AMOUNT,
+    LEVEL
+};
